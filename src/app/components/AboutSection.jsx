5e0c99210e5bbd8534ee6a3f11633569fa6e1017
himanshu1004200 <chaharhimanshu03@gmail.com>
2024-02-07 08:25:29 +0530

    first commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,106 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+         <li>C++</li>
+        <li>NextJs</li>
+        <li>TypeScript</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>NodeJs</li>
+        <li>Express</li>
+
+       
+
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Computer Science Engineering</li>
+        <li>Bennett University, Greater Noida</li>
+        <li>CGPA - 8.91</li>
+
+      </ul>
+    ),
+  },
+  {
+    title: "Achievements",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>1st in Project Showcase organised by Bennett</li>
+        <li>Under 20 in Coda-thon</li>
+        <li>LeetCode - 250+</li>
+
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a full stack web developer with a passion for creating
+            interactive and responsive web applications. I have experience
+            working with <strong>JavaScript, TypeScript, ReactJs, NextJs, Redux, Node.js, Express, Websockets, HTML, CSS, C++ and Git</strong>. I am a quick learner and I am always
+            looking to expand my knowledge and skill set. I am a team player and
+            I am excited to work with others to create amazing applications.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Achievements{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
