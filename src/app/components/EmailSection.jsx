8065542065ad349d8e21e6d54ba1c34c5045a02b
himanshu1004200 <chaharhimanshu03@gmail.com>
2024-02-11 20:12:04 +0530

    Some Basic Changes!

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -44,10 +44,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/chaharhimanshu1004" target="_blank">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link href="https://www.linkedin.com/in/himanshu-chahar-68605123a/" target="_blank">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -24,7 +24,7 @@ const HeroSection = () => {
               sequence={[
                 "Web Developer",
                 1000,
-                "DSA Ninja",
+                "DSA Enthusiast",
                 1000,
               ]}
               wrapper="span"
@@ -43,9 +43,11 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+              href="https://drive.google.com/file/d/1yH-CfdMfMxoKLb1lIQiBhc-Y576OIsH-/view?usp=drive_link" target="_blank"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3
+              "
             >
+              
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
